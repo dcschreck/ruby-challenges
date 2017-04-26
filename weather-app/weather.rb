eca26c39e5939c7b5747da01c1e444b014ce1b17
Douglas Schreck <dcschreck421@gmail.com>
2017-04-26 19:31:58 -0400

    weather.rb

---
+++ weather-app/weather.rb
@@ -0,0 +1,28 @@
+require "barometer"
+
+puts "Where you at?"
+location = gets
+
+barometer = Barometer.new(location)
+weather = barometer.measure
+
+current = weather.current.temperature.f.to_str
+
+def get_locations_weather(location)
+  Barometer.new(location).measure
+end
+
+weather = get_locations_weather(location)
+tomorrow = Time.now.strftime('%d').to_i + 1
+
+weather.forecast.each do |forecast|
+  day = forecast.starts_at.day
+
+  if day == tomorrow
+    dayName = 'Tomorrow'
+  else
+    dayName = forecast.starts_at.strftime('%A')
+  end
+
+  puts dayName + ' is going to be ' + forecast.icon + ' with a low of ' + forecast.low.f.to_s + ' and a high of ' + forecast.high.f.to_s
+end
